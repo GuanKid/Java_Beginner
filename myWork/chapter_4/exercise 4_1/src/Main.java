a26cfd5a1ec8feef5712523ac4d5541e0ec88ae5
Desire Gwanzura <112183262+GuanKid@users.noreply.github.com>
2024-09-18 14:41:13 +0300

    Delete myWork directory

--- myWork/chapter_4/exercise 4_1/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String custName ="Sire", itemDesc = "Book";
-
-        String message;
-        message = custName + " wants to purchase a " + itemDesc;
-        System.out.println(message);
-    }
-}
--- myWork/chapter_6/exercise_2/myWork/src/item.java
+++
@@ -1,7 +0,0 @@
-public class item {
-
-    public int itemID;
-    public String desc;
-    public double price;
-    public int quantity;
-}
